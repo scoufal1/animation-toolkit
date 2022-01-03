dc40010257ea497c1cf60625f5d2c0da7d0e76df
Sarah Coufal <sarah.coufal1@gmail.com>
2022-01-03 17:12:40 -0500

    skeleton practice

--- assignments/a6-transform/skeleton_practice.cpp
+++ assignments/a6-transform/skeleton_practice.cpp
@@ -3,7 +3,7 @@
 using namespace atk;
 using glm::vec3;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
    Skeleton skeleton;
    Motion motion;
@@ -11,37 +11,37 @@ int main(int argc, char** argv)
    BVHReader reader;
    reader.load("../motions/Beta/idle.bvh", skeleton, motion);
 
-   int numJoints = 0; 
+   int numJoints = skeleton.getNumJoints();
    std::cout << "The number of joints is " << numJoints << std::endl;
 
    // todo: loop through and print all joint names
    for (int i = 0; i < numJoints; i++)
    {
       // todo
-      std::string jointName = "";
+      std::string jointName = skeleton.getByID(i)->getName();
       std::cout << i << " " << jointName << std::endl;
    }
 
    // todo get root of the skeleton
-   //Joint* root = NULL;
-   //std::cout << "The root joint name is: " << rootName << std::endl;
-   //std::cout << "The root joint ID is: " << rootID << std::endl;
-   //std::cout << "The root joint's number of children is: " << numchildren << std::endl;
+   Joint *root = skeleton.getRoot();
+   std::cout << "The root joint name is: " << root->getName() << std::endl;
+   std::cout << "The root joint ID is: " << root->getID() << std::endl;
+   std::cout << "The root joint's number of children is: " << root->getNumChildren() << std::endl;
 
    // todo print the names of the children of the root
-   int rootNumChildren = 0;
+   int rootNumChildren = root->getNumChildren();
    for (int i = 0; i < rootNumChildren; i++)
    {
-      std::string childName = "";
-      std::cout << childName << std::endl; 
-   } 
+      std::string childName = root->getChildAt(i)->getName();
+      std::cout << childName << std::endl;
+   }
 
    // Get the joint for the hand by looking up by name ("Beta:LeftHand")
-   Joint* hand = NULL;
-   vec3 offset; // todo get the local offset from the hand to its parent 
+   Joint *hand = skeleton.getByName("Beta:LeftHand");
+   vec3 offset = hand->getLocalTranslation(); // todo get the local offset from the hand to its parent
    std::cout << "The offset between the left hand and its parent is " << glm::to_string(offset) << std::endl;
 
    // todo: get the hand's parent joint's name
-   std::string handParentName = "";
+   std::string handParentName = hand->getParent()->getName();
    std::cout << "The parent of the hand is " << handParentName << std::endl;
 }
