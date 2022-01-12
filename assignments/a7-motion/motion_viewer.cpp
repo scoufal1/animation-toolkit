0955b1dc51160676331f10914c7a479bfd4aac56
Sarah Coufal <sarah.coufal1@gmail.com>
2022-01-11 21:15:05 -0500

    motion viewer

--- assignments/a7-motion/motion_viewer.cpp
+++ assignments/a7-motion/motion_viewer.cpp
@@ -8,35 +8,84 @@ using namespace atk;
 class MotionViewer : public atkui::Framework
 {
 public:
-   MotionViewer() : atkui::Framework(atkui::Perspective) {
+   MotionViewer() : atkui::Framework(atkui::Perspective)
+   {
    }
 
-   void setup() {
+   void setFileName(std::string file)
+   {
+      filename = file;
+   }
+
+   void setup()
+   {
       BVHReader reader;
-      reader.load("../motions/Beta/jump.bvh", skeleton, motion);
+      reader.load(filename, skeleton, motion);
       motion.update(skeleton, 0);
    }
 
-   void scene() {
-      time += dt();
+   void scene()
+   {
+      if (!paused)
+      {
+         time += dt() * timeScale;
+      }
+      currentFrame = motion.getKeyID(time);
       motion.update(skeleton, time);
 
-      setColor(vec3(0,0,0.8));
-      for (int i = 0; i < skeleton.getNumJoints(); i++) {
-         Joint* joint = skeleton.getByID(i);
-         if (joint->getParent() == 0) continue;
+      setColor(vec3(0, 0, 0.8));
+      for (int i = 0; i < skeleton.getNumJoints(); i++)
+      {
+         Joint *joint = skeleton.getByID(i);
+         if (joint->getParent() == 0)
+            continue;
 
          vec3 p1 = joint->getGlobalTranslation();
          vec3 p2 = joint->getParent()->getGlobalTranslation();
          drawEllipsoid(p1, p2, 5);
       }
 
-      drawText(paused? "Paused" : "Playing", 10, 15);
-      drawText("Current frame: "+std::to_string(currentFrame), 10, 35);
-      drawText("Time scale: "+std::to_string(timeScale), 10, 55);
+      drawText(paused ? "Paused" : "Playing", 10, 15);
+      drawText("Current frame: " + std::to_string(currentFrame), 10, 35);
+      drawText("Time scale: " + std::to_string(timeScale), 10, 55);
    }
 
-   virtual void keyUp(int key, int mods) {
+   virtual void keyUp(int key, int mods)
+   {
+      if (key == 'P')
+      {
+         paused = !paused;
+      }
+      if (key == '0')
+      {
+         time = 0;
+         currentFrame = 0;
+         skeleton.setPose(motion.getKey(currentFrame));
+      }
+      if (paused && key == '.')
+      {
+         time += motion.getDeltaTime();
+      }
+      if (paused && key == ',')
+      {
+         time -= motion.getDeltaTime();
+         if (time < 0)
+         {
+            time = motion.getDuration() - motion.getDeltaTime();
+         }
+      }
+      if (key == ']')
+      {
+         timeScale += 0.1;
+      }
+      if (key == '[')
+      {
+         timeScale -= 0.1;
+         if (timeScale < 0.1)
+         {
+            timeScale = 0.1f;
+         }
+      }
    }
 
 private:
@@ -44,13 +93,24 @@ private:
    Motion motion;
 
    float timeScale = 1.0f;
-   int currentFrame = 0; 
+   int currentFrame = 0;
    bool paused = false;
    float time = 0;
+   std::string filename;
 };
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
    MotionViewer viewer;
+   if (argc > 1)
+   {
+      viewer.setFileName(argv[1]);
+   }
+   // default motion
+   else
+   {
+      viewer.setFileName("../motions/Beta/jump.bvh");
+   }
    viewer.run();
 }
